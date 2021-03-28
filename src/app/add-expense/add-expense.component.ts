78bd1fba09d9881cef04b4f400a2c4089d04a5b6
Nick Stigsell <nick@stigsell.com>
2021-03-28 15:14:22 -0500

    Basic add expense component working

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-add-expense',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-expense.component.css']
 })
 export class AddExpenseComponent implements OnInit {
+	checkoutForm = this.formBuilder.group({
+    	date: '',
+    	place: '',
+    	amount: '',
+    	description: '',
+    	category: '',
+    	subcategory: ''
+  	});
+  	data = {
+  		date: "",
+  		place: "",
+  		amount: "",
+  		description: "",
+  		category: "",
+  		subcategory: ""
+  	};
+ 	constructor(
+  		private formBuilder: FormBuilder
+  		) { }
 
-  constructor() { }
+  	ngOnInit(): void {
+  	}
 
-  ngOnInit(): void {
+  	onSubmit(): void {
+    // Process checkout data here
+    // this.items = this.cartService.clearCart();
+    console.log(this.checkoutForm.value);
+    this.data = this.checkoutForm.value;
+    console.log(this.data);
+    this.checkoutForm.reset();
   }
 
 }
