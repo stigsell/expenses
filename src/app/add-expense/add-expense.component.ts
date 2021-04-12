9994397af23aa794d2fdcb77b7fab0fe6a938fe5
Nick Stigsell <nick@stigsell.com>
2021-04-11 23:08:32 -0500

    Made add expense and widget components query/mutate GraphQL. Made widgets always display 2 decimal places

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { LocalStorageService } from '../local-storage.service';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-add-expense',
@@ -18,42 +19,43 @@ export class AddExpenseComponent implements OnInit {
     	category: '',
     	subcategory: ''
   	});
-  	expenses = [{
-  		date: "",
-  		place: "",
-  		amount: 0.0,
-  		description: "",
-  		category: "",
-  		subcategory: ""
-  	}];
+  ADD_EXPENSE = gql`
+    mutation CreateExpense($d: String!, $p: String!, $a: String!, $ds: String!, $c: String!, $s: String) {
+      createExpense(date: $d, place: $p, amount: $a, description: $ds, category: $c, subcategory:$s) {
+      id
+    }
+}
+  `;
+
  	constructor(
   		private formBuilder: FormBuilder,
-  		private localStorageService: LocalStorageService
+  		private localStorageService: LocalStorageService,
+      private apollo: Apollo
 
   		) { }
 
   	ngOnInit(): void {
-  		this.expenses = this.localStorageService.get("expenses");
-  		if (Object.keys(this.expenses).length === 0) {
-  			this.expenses = [];
-  		}
+
   	}
 
-  	onSubmit(): void {
-    if (this.addExpenseForm.value.date != "" &&
-      this.addExpenseForm.value.place != "" && 
-      this.addExpenseForm.value.amount != "" && 
-      this.addExpenseForm.value.description != "" && 
-      this.addExpenseForm.value.category != "") {
-        console.log(this.addExpenseForm.value);
-        console.log(this.expenses);
-        this.expenses.push(this.addExpenseForm.value);
-        this.localStorageService.set("expenses", this.expenses);
-        console.log(this.expenses);
-        this.addExpenseForm.reset();
-        location.reload();
+    onSubmit(): void {
+      this.apollo.mutate({
+        mutation: this.ADD_EXPENSE,
+        variables: {
+          d: this.addExpenseForm.value.date,
+          p: this.addExpenseForm.value.place,
+          a: this.addExpenseForm.value.amount,
+          ds: this.addExpenseForm.value.description,
+          c: this.addExpenseForm.value.category,
+          s: this.addExpenseForm.value.subcategory
+        }
+      }).subscribe(({data}) => {
+        console.log(data);
+        location.reload();  // TODO don't refresh the page visually to the user
+        // TODO provide visual confirmation to user that expense was added successfully
+      }, (error) => {
+        console.log("Error", error);
+      });
     }
-    
-  }
 
 }
--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../local-storage.service';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-widget',
@@ -7,35 +8,56 @@ import { LocalStorageService } from '../local-storage.service';
   styleUrls: ['./widget.component.css']
 })
 export class WidgetComponent implements OnInit {
-	expenses = [{
-  		date: "",
-  		place: "",
-  		amount: 0.0,
-  		description: "",
-  		category: "",
-  		subcategory: ""
-  	}];
+    expenses: any[];
+    loading = true;
+    error: any;
+
+
+    GET_ALL_EXPENSES = gql`
+      query GetAllExpenses {
+        expenses {
+          id
+          date
+          place
+          amount
+          description
+          category
+          subcategory
+        }
+      }
+    `;
   	totalExpenses: number = 0;
   	averageExpense: number = 0;
   	constructor(
-  		private localStorageService: LocalStorageService
-  		) { }
+  		private localStorageService: LocalStorageService,
+      private apollo: Apollo
+  		) { 
+      this.expenses = [];
+    }
 
   	ngOnInit(): void {
-  		this.expenses = this.localStorageService.get("expenses");
-  		this.totalExpenses = this.getTotalExpenses();
-  		this.averageExpense = this.totalExpenses / this.expenses.length;
+      this.apollo.watchQuery({
+        query: this.GET_ALL_EXPENSES,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.expenses = result?.data?.expenses;
+        this.loading = result.loading;
+        this.error = result.error;
+        this.totalExpenses = this.getTotalExpenses();
+        this.averageExpense = this.totalExpenses / this.expenses.length;
+        console.log(this.averageExpense);
+      })
+  		// this.expenses = this.localStorageService.get("expenses");
+  		
   }
   	getTotalExpenses(): number {
   		let sum = 0;
-  		console.log(sum);
   		this.expenses.forEach(expense => {
   			if (isNaN(Number(expense.amount))) {
   				console.log("Not a number: " + expense.amount)
   			}
   			sum = sum + Number(expense.amount);
   		})
-  		console.log(sum);
   		return sum;
   	}
 
