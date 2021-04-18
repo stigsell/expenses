a098f41703d33a967d483e3ec5d1611369b73a5a
Nick Stigsell <nick@stigsell.com>
2021-04-17 19:36:17 -0500

    Moved graphql query/mutate to separate services instead of being in expenses table component

--- src/app/expenses-table/expenses-table.component.ts
+++ src/app/expenses-table/expenses-table.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { LocalStorageService } from '../local-storage.service';
+import { Expense, GetAllExpensesService } from '../get-all-expenses.service';
+import { DeleteExpenseService } from '../delete-expense.service';
 import { EditExpenseComponent } from '../edit-expense/edit-expense.component';
-import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-expenses-table',
@@ -10,70 +12,35 @@ import { Apollo, gql } from 'apollo-angular';
   styleUrls: ['./expenses-table.component.css']
 })
 export class ExpensesTableComponent implements OnInit {
- 	expenses: any[];
+ 	expenses!: Observable<Expense[]>;
   loading = true;
   error: any;
   showButton = false;
   buttonId: Number;
 
-
-  GET_ALL_EXPENSES = gql`
-    query GetAllExpenses {
-      expenses {
-        id
-        date
-        place
-        amount
-        description
-        category
-        subcategory
-      }
-    }
-  `;
-
-  DELETE_EXPENSE = gql`
-    mutation DeleteExpense($id: Int!) {
-      deleteExpense(id: $id) {
-      success
-    }
-  }
-  `;
 	constructor(
-		private localStorageService: LocalStorageService,
-    private apollo: Apollo
+		private getAllExpensesService: GetAllExpensesService,
+    private deleteExpenseService: DeleteExpenseService
 		) { 
-      this.expenses = [];
       this.buttonId = -1;
   }
 
   ngOnInit(): void {
-    this.apollo.watchQuery({
-      query: this.GET_ALL_EXPENSES,
-    })
-    .valueChanges.subscribe((result: any) => {
-      this.expenses = result?.data?.expenses;
-      this.loading = result.loading;
-      this.error = result.error;
-      console.log(this.expenses);
-    })
+    this.expenses = this.getAllExpensesService.watch()
+      .valueChanges
+      .pipe(
+        map(result => result.data.expenses)
+        );
 
   }
 
   deleteRow(id: Number): void {
-    console.log(id);
-    this.buttonId = id;
-    this.apollo.mutate({
-        mutation: this.DELETE_EXPENSE,
-        variables: {
-          id: id
-        }
-      }).subscribe(({data}) => {
-        console.log(data);
-        location.reload();  // TODO don't refresh the page visually to the user
-        // TODO provide visual confirmation to user that expense was added successfully
-      }, (error) => {
-        console.log("Error", error);
-      });
+    this.deleteExpenseService
+      .mutate({
+        id: id
+      })
+      .subscribe();
+     location.reload();  // TODO don't refresh the page visually to the user
   }
 
 }
